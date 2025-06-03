16024642d3c35e0235aa191d3baf4969c23e3fa6
JunHo <jjockrod@naver.com>
2025-06-03 22:32:31 +0900

    [KAN-82] swagger (#39)

--- src/hb-backend-api/daily-todo/adapters/in/dto/get-daily-todo.dto.ts
+++ src/hb-backend-api/daily-todo/adapters/in/dto/get-daily-todo.dto.ts
@@ -1,6 +1,7 @@
 import { DailyTodoCompleteStatus } from "../../../domain/enums/daily-todo-complete-status.enum";
 import { DailyTodoCycle } from "../../../domain/enums/daily-todo-cycle.enum";
 import { DailyTodoWithRelationQueryResult } from "../../../application/result/daily-todo-query.result";
+import { ApiProperty } from "@nestjs/swagger";
 
 interface OwnerType {
   id: string;
@@ -18,16 +19,67 @@ interface ReactionType {
   reactionUserId: string;
 }
 
+class ReactionDto {
+  @ApiProperty({ type: String })
+  value: string;
+
+  @ApiProperty({ type: String })
+  reactionUserId: string;
+}
+
+class OwnerDto {
+  @ApiProperty({ type: String })
+  id: string;
+
+  @ApiProperty({ type: String })
+  username: string;
+
+  @ApiProperty({ type: String })
+  nickname: string;
+}
+
+class CategoryDto {
+  @ApiProperty({ type: String })
+  id: string;
+
+  @ApiProperty({ type: String })
+  title: string;
+}
+
 export class GetDailyTodoDto {
+  @ApiProperty({ type: String })
+  id: string;
+
+  @ApiProperty({ type: String })
+  title: string;
+
+  @ApiProperty({ type: String })
+  date: Date;
+
+  @ApiProperty({ type: ReactionDto || null })
+  reaction: ReactionType | null;
+
+  @ApiProperty({ enum: DailyTodoCompleteStatus })
+  progress: DailyTodoCompleteStatus;
+
+  @ApiProperty({ enum: DailyTodoCycle })
+  cycle: DailyTodoCycle;
+
+  @ApiProperty({ type: OwnerDto })
+  owner: OwnerType;
+
+  @ApiProperty({ type: CategoryDto })
+  category: CategoryType;
+
   constructor(
-    private readonly id: string,
-    private readonly title: string,
-    private readonly date: Date,
-    private readonly reaction: ReactionType | null,
-    private readonly progress: DailyTodoCompleteStatus,
-    private readonly cycle: DailyTodoCycle,
-    private readonly owner: OwnerType,
-    private readonly category: CategoryType,
+    id: string,
+    title: string,
+    date: Date,
+    reaction: ReactionType | null,
+    progress: DailyTodoCompleteStatus,
+    cycle: DailyTodoCycle,
+    owner: OwnerType,
+    category: CategoryType,
   ) {
     this.id = id;
     this.title = title;
