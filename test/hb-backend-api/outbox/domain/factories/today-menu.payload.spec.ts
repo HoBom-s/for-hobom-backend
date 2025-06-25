c7422d50bb997a684dfdb7be93996f3ab9f6f793
foxmon <jjockrod@naver.com>
2025-06-25 21:17:15 +0900

    [KAN-127] today menu pick outbox payload

--- test/hb-backend-api/outbox/domain/factories/today-menu.payload.spec.ts
+++ test/hb-backend-api/outbox/domain/factories/today-menu.payload.spec.ts
@@ -3,14 +3,22 @@ import { createTodayMenuPayload } from "../../../../../src/hb-backend-api/outbox
 describe("today-menu.payload", () => {
   describe("createTodayMenuPayload()", () => {
     it("should return an today menu payload object", () => {
-      const reusult = createTodayMenuPayload({
+      const result = createTodayMenuPayload({
         todayMenuId: "1",
         name: "today-menu",
+        username: "user",
+        nickname: "nickname",
+        email: "email",
+        userId: "userId",
       });
 
-      expect(reusult).toStrictEqual({
+      expect(result).toStrictEqual({
         todayMenuId: "1",
         name: "today-menu",
+        username: "user",
+        nickname: "nickname",
+        email: "email",
+        userId: "userId",
       });
     });
   });
