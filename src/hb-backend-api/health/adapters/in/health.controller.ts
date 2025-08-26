17e45fe7eb95e9521290b5a898064a53d19fdf96
foxmon <jjockrod@naver.com>
2025-08-26 22:33:54 +0900

    [KAN-157] health

--- src/hb-backend-api/health/adapters/in/health.controller.ts
+++ src/hb-backend-api/health/adapters/in/health.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get } from "@nestjs/common";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 
+@ApiTags("Health Check")
 @Controller()
 export class HealthController {
+  @ApiOperation({ summary: "헬스 체크", description: "헬스 체크 API" })
   @Get("")
   public ok() {
     return "OK";
